bf3a942a77df69416dc1cdb8a92c1012d2428eb3
Mahaub Alam <mahabubalom72091@gmail.com>
2025-06-26 09:55:54 +0300

    added all data

---
+++ client/src/components/Categories/CategoriesData.js
@@ -0,0 +1,93 @@
+import { TbBeach, TbMountain, TbPool } from 'react-icons/tb'
+import {
+  GiBarn,
+  GiBoatFishing,
+  GiCactus,
+  GiCastle,
+  GiCaveEntrance,
+  GiForestCamp,
+  GiIsland,
+  GiWindmill,
+} from 'react-icons/gi'
+import { FaSkiing } from 'react-icons/fa'
+import { BsSnow } from 'react-icons/bs'
+import { IoDiamond } from 'react-icons/io5'
+import { MdOutlineVilla } from 'react-icons/md'
+
+export const categories = [
+  {
+    label: "Roads Gorund",
+    icon: TbBeach,
+    description: "This property is close to the beach!",
+  },
+  {
+    label: "Royel-Terminal",
+    icon: GiWindmill,
+    description: "This property is has windmills!",
+  },
+  {
+    label: "Inside-terminal",
+    icon: MdOutlineVilla,
+    description: "This property is modern!",
+  },
+  {
+    label: "GVIP",
+    icon: TbMountain,
+    description: "This property is in the countryside!",
+  },
+  {
+    label: "Control-Twoer",
+    icon: TbPool,
+    description: "This is property has a beautiful pool!",
+  },
+  {
+    label: "North/South",
+    icon: GiIsland,
+    description: "This property is on an island!",
+  },
+  {
+    label: "Parking-Grage",
+    icon: GiBoatFishing,
+    description: "This property is near a lake!",
+  },
+  {
+    label: "Irrigation",
+    icon: FaSkiing,
+    description: "This property has skiing activities!",
+  },
+  {
+    label: "WorksShop",
+    icon: GiCastle,
+    description: "This property is an ancient castle!",
+  },
+  {
+    label: "Managment",
+    icon: GiCaveEntrance,
+    description: "This property is in a spooky cave!",
+  },
+  {
+    label: "Nursery",
+    icon: GiForestCamp,
+    description: "This property offers camping activities!",
+  },
+  {
+    label: "Campe",
+    icon: BsSnow,
+    description: "This property is in arctic environment!",
+  },
+  {
+    label: "Messhall",
+    icon: GiCactus,
+    description: "This property is in the desert!",
+  },
+  {
+    label: "Wast-Managmnet",
+    icon: GiBarn,
+    description: "This property is in a barn!",
+  },
+  {
+    label: "Street-Light",
+    icon: IoDiamond,
+    description: "This property is brand new and luxurious!",
+  },
+];
